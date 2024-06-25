d3e8b17ba2254c70faefab4fb3dbef527dc36140
albinabelaeva <134770106+albinabelaeva@users.noreply.github.com>
2024-06-25 11:41:40 +0700

    the job is ready

---
+++ app/src/main/java/com/example/a11pract/MainActivity3.java
@@ -0,0 +1,32 @@
+package com.example.a11pract;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+public class MainActivity3 extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // Загрузка веб-страницы
+        webView.loadUrl("https://developer.android.com"); // Замените URL на тот, который вам нужен
+        setContentView(R.layout.activity_main3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+}
